a678792a1324e2a851dc4ac76e268ff719e8f9bf
kmova <kiran.mova@cloudbyte.com>
2017-06-21 14:54:18 +0530

    initial commit for openebs-provisioner

---
+++ openebs-provisioner.go
@@ -0,0 +1,150 @@
+/*
+Copyright 2016-2017 The Kubernetes Authors.
+Copyright 2016-2017 The OpenEBS Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package main
+
+import (
+	"errors"
+	"flag"
+	"os"
+	"time"
+
+	"github.com/golang/glog"
+	"github.com/kubernetes-incubator/external-storage/lib/controller"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/wait"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/pkg/api/v1"
+	"k8s.io/client-go/rest"
+	"syscall"
+)
+
+const (
+	resyncPeriod              = 15 * time.Second
+	provisionerName           = "openebs.io/provisioner-iscsi"
+	exponentialBackOffOnError = false
+	failedRetryThreshold      = 5
+	leasePeriod               = controller.DefaultLeaseDuration
+	retryPeriod               = controller.DefaultRetryPeriod
+	renewDeadline             = controller.DefaultRenewDeadline
+	termLimit                 = controller.DefaultTermLimit
+)
+
+type openEBSProvisioner struct {
+	// Maya-API Server URI running in the cluster
+	mapiURI string
+
+	// Identity of this openEBSProvisioner, set to node's name. Used to identify
+	// "this" provisioner's PVs.
+	identity string
+}
+
+// NewOpenEBSProvisioner creates a new openebs provisioner
+func NewOpenEBSProvisioner() controller.Provisioner {
+	nodeName := os.Getenv("NODE_NAME")
+	if nodeName == "" {
+		glog.Fatal("env variable NODE_NAME must be set so that this provisioner can identify itself")
+	}
+	
+	//TODO - Replace with the code to dynamically fetch the MAPI_CLUSTER_IP
+	return &openEBSProvisioner{
+		mapiURI:    "http://MAPI_CLUSTER_IP:5656/",
+		identity: nodeName,
+	}
+}
+
+var _ controller.Provisioner = &openEBSProvisioner{}
+
+// Provision creates a storage asset and returns a PV object representing it.
+func (p *openEBSProvisioner) Provision(options controller.VolumeOptions) (*v1.PersistentVolume, error) {
+	path := options.PVName
+
+	//TODO - Issue a request to Maya API Server to create a volume
+	//TODO - fill in the iSCSI PV details
+	pv := &v1.PersistentVolume{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: options.PVName,
+			Annotations: map[string]string{
+				"openEBSProvisionerIdentity": p.identity,
+			},
+		},
+		Spec: v1.PersistentVolumeSpec{
+			PersistentVolumeReclaimPolicy: options.PersistentVolumeReclaimPolicy,
+			AccessModes:                   options.PVC.Spec.AccessModes,
+			Capacity: v1.ResourceList{
+				v1.ResourceName(v1.ResourceStorage): options.PVC.Spec.Resources.Requests[v1.ResourceName(v1.ResourceStorage)],
+			},
+			PersistentVolumeSource: v1.PersistentVolumeSource{
+				HostPath: &v1.HostPathVolumeSource{
+					Path: path,
+				},
+			},
+		},
+	}
+
+	return pv, nil
+}
+
+// Delete removes the storage asset that was created by Provision represented
+// by the given PV.
+func (p *openEBSProvisioner) Delete(volume *v1.PersistentVolume) error {
+	ann, ok := volume.Annotations["openEBSProvisionerIdentity"]
+	if !ok {
+		return errors.New("identity annotation not found on PV")
+	}
+	if ann != p.identity {
+		return &controller.IgnoredError{Reason: "identity annotation on PV does not match ours"}
+	}
+
+	//TODO - Issue a delete request to Maya API Server
+
+	return nil
+}
+
+func main() {
+	syscall.Umask(0)
+
+	flag.Parse()
+	flag.Set("logtostderr", "true")
+
+	// Create an InClusterConfig and use it to create a client for the controller
+	// to use to communicate with Kubernetes
+	config, err := rest.InClusterConfig()
+	if err != nil {
+		glog.Fatalf("Failed to create config: %v", err)
+	}
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		glog.Fatalf("Failed to create client: %v", err)
+	}
+
+	// The controller needs to know what the server version is because out-of-tree
+	// provisioners aren't officially supported until 1.5
+	serverVersion, err := clientset.Discovery().ServerVersion()
+	if err != nil {
+		glog.Fatalf("Error getting server version: %v", err)
+	}
+
+	// Create the provisioner: it implements the Provisioner interface expected by
+	// the controller
+	openEBSProvisioner := NewOpenEBSProvisioner()
+
+	// Start the provision controller which will dynamically provision OpenEBS VSM
+	// PVs
+	pc := controller.NewProvisionController(clientset, resyncPeriod, provisionerName, openEBSProvisioner, serverVersion.GitVersion, exponentialBackOffOnError, failedRetryThreshold, leasePeriod, renewDeadline, retryPeriod, termLimit)
+	pc.Run(wait.NeverStop)
+}
